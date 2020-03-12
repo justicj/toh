3340a62fa690046c9923781e35b16e08d8b275db
Jason Justice <jason.justice@viasat.com>
2020-03-12 16:55:35 -0700

    start of position tracking.

--- toh.py
+++ toh.py
@@ -1,8 +1,8 @@
 # Recursive Python function to solve tower of hanoi
 
 def TowerOfHanoi(n , from_rod, to_rod, aux_rod):
-    global its
-    its += 1
+    global moves
+    moves += 1
     if n == 1:
         print("Move disk 1 from rod",from_rod,"to rod",to_rod)
         return
@@ -10,10 +10,19 @@ def TowerOfHanoi(n , from_rod, to_rod, aux_rod):
     print("Move disk",n,"from rod",from_rod,"to rod",to_rod)
     TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
 
+def DrawTowers(a, b, c):
+    #init all on A rod
+    print(a, b, c)
+
+
 # Driver code
-n = 4
-its = 0
+n = 3
+moves = 0
+#draw first towers
+DrawTowers(n, 0, 0)
 TowerOfHanoi(n, 'A', 'C', 'B')
 # A, C, B are the name of rods
-print("Number of moves: "+ str(its))
+print("Number of moves: "+ str(moves))
 # Contributed By Harshit Agrawal
+#
+#
