a79c6c176ddf195026d9481720f4d23d38218ffa
Jason Justice <jason.justice@viasat.com>
2020-03-12 17:19:42 -0700

    working to show how many discs on each rod

--- toh.py
+++ toh.py
@@ -1,13 +1,62 @@
 # Recursive Python function to solve tower of hanoi
 
 def TowerOfHanoi(n , from_rod, to_rod, aux_rod):
+    global a, b, c
     global moves
     moves += 1
     if n == 1:
         print("Move disk 1 from rod",from_rod,"to rod",to_rod)
+        if from_rod == 'A' and to_rod == 'B':
+            a -= 1
+            b += 1
+            DrawTowers(a, b, c)
+        elif from_rod == 'A' and to_rod == 'C':
+            a -= 1
+            c += 1
+            DrawTowers(a, b, c)
+        elif from_rod == 'B' and to_rod == 'A':
+            a += 1
+            b -= 1
+            DrawTowers(a, b, c)
+        elif from_rod == 'B' and to_rod == 'C':
+            c += 1
+            b -= 1
+            DrawTowers(a, b, c)
+        elif from_rod == 'C' and to_rod == 'A':
+            a += 1
+            c -= 1
+            DrawTowers(a, b, c)
+        elif from_rod == 'C' and to_rod == 'B':
+            b += 1
+            c -= 1
+            DrawTowers(a, b, c)
         return
     TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
     print("Move disk",n,"from rod",from_rod,"to rod",to_rod)
+    if from_rod == 'A' and to_rod == 'B':
+        a -= 1
+        b += 1
+        DrawTowers(a, b, c)
+    elif from_rod == 'A' and to_rod == 'C':
+        a -= 1
+        c += 1
+        DrawTowers(a, b, c)
+    elif from_rod == 'B' and to_rod == 'A':
+        a += 1
+        b -= 1
+        DrawTowers(a, b, c)
+    elif from_rod == 'B' and to_rod == 'C':
+        c += 1
+        b -= 1
+        DrawTowers(a, b, c)
+    elif from_rod == 'C' and to_rod == 'A':
+        a += 1
+        c -= 1
+        DrawTowers(a, b, c)
+    elif from_rod == 'C' and to_rod == 'B':
+        b += 1
+        c -= 1
+        DrawTowers(a, b, c)
     TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
 
 def DrawTowers(a, b, c):
@@ -16,10 +65,13 @@ def DrawTowers(a, b, c):
 
 
 # Driver code
-n = 3
+n = 5
 moves = 0
 #draw first towers
-DrawTowers(n, 0, 0)
+a = n
+b = 0
+c = 0
+DrawTowers(a, b, c)
 TowerOfHanoi(n, 'A', 'C', 'B')
 # A, C, B are the name of rods
 print("Number of moves: "+ str(moves))
